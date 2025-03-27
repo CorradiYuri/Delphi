cfaae3bf14b2d143e05fc3ac403f2d5bf04c676f
CorradiYuri <yuricorradi2002@gmail.com>
2025-03-27 19:32:03 -0300

    Exercicio Visual

---
+++ exerciciovisual/uprincipal.pas
@@ -0,0 +1,57 @@
+unit uprincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.ComCtrls,
+  Vcl.Mask, Vcl.DBCtrls, Vcl.WinXCtrls, Vcl.Skia, Vcl.Imaging.jpeg;
+
+type
+  TForm1 = class(TForm)
+    Meuedit: TEdit;
+    RadioGroup: TRadioGroup;
+    Item1: TRadioButton;
+    Item2: TRadioButton;
+    Iten3: TRadioButton;
+    CheckBox: TCheckBox;
+    Painel2: TLabel;
+    GoupBox: TGroupBox;
+    Memo: TMemo;
+    lst1: TListBox;
+    checkListBox1: TCheckBox;
+    checkListBox2: TCheckBox;
+    checkListBox3: TCheckBox;
+    checkListBox4: TCheckBox;
+    Telefone: TEdit;
+    acer: TEdit;
+    zero: TEdit;
+    clBlack: TEdit;
+    groupbox: TGroupBox;
+    button2: TButton;
+    button3: TButton;
+    checkBox2: TCheckBox;
+    comboBox1: TComboBox;
+    rechEdit: TRichEdit;
+    selecionedata: TComboBox;
+    cal1: TMonthCalendar;
+    button: TButton;
+    emBranco: TComboBox;
+    lst2: TColorListBox;
+    tglswtch1: TToggleSwitch;
+    LabeleEdit: TDBLabeledEdit;
+    img1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
